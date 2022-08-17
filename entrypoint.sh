2cf1c1b731749242630c40f37d6a465223c0c37a
Matthew Connelly <Matthew.Connelly@fanduel.com>
2022-08-17 16:23:26 +0100

    entrypoint: Auto-install requirements if any
    If /data/requirements exists, install all requirements listed in it
    before starting any commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,12 @@ JUPYDIR=/root/.jupyter
 
 JUPYDEF=/.default.py
 JUPYCNF=$JUPYDIR/jupyter_lab_config.py
+JUREQFL=$DATADIR/requirements
 
 cd $DATADIR
 . $VENVDIR/bin/activate
 
 [ ! -f $JUPYCNF ] && cp $JUPYDEF $JUPYCNF
+[ -f $JUREQFL ] && pip install -Ur $JUREQFL
 
 ( $@ )
